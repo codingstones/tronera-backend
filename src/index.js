edbe023beaddd5dd46029b36424cccec61ea69e6
Néstor Salceda <nestor.salceda@gmail.com>
2017-05-08 00:12:05 +0200

    Retrieve tweets without twitter library

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-const Twitter = require('twitter');
+require('isomorphic-fetch');
+
+class Twitter {
+  constructor(options) {
+    this.baseUrl = "https://api.twitter.com/1.1/"
+
+    this.consumerKey = options.consumerKey;
+    this.consumerSecret = options.consumerSecret;
+    this.accessTokenKey = options.accessTokenKey;
+    this.accessTokenSecret = options.accessTokenSecret;
+  }
+
+  get(endpoint, params) {
+    return this._tokenForApplication().then((response) => {
+      const access_token = response.access_token;
+      const url = this.baseUrl + endpoint + '.json' + this._toQueryString(params);
+
+      return fetch(url, {
+        headers: new Headers({
+          "Authorization": "Bearer " + access_token,
+        })
+      }).then((response) => {
+        return response.json();
+      });
+    });
+  }
+
+  _tokenForApplication() {
+    return fetch("https://api.twitter.com/oauth2/token", {
+      method: "POST",
+      headers: new Headers({
+        "Authorization": "Basic " + this._encodeConsumerKeyAndSecret(),
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+      }),
+      body: "grant_type=client_credentials"
+    }).then((response) => {
+      return response.json();
+    });
+  }
+
+  _encodeConsumerKeyAndSecret() {
+    //FIXME: Use an isomorphic btoa function
+    return new Buffer(this.consumerKey + ':' + this.consumerSecret).toString('base64');
+  }
+
+  _toQueryString(object) {
+    return '?' + Object.keys(object).map((key) => {
+      return encodeURIComponent(key) + '=' + encodeURIComponent(object[key]);
+    }).join('&');
+  }
+}
 
 class TwitterService {
   constructor() {
     this.client = new Twitter({
-      consumer_key: process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+      consumerKey: process.env.TWITTER_CONSUMER_KEY,
+      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+      accessTokenKey: process.env.TWITTER_ACCESS_TOKEN_KEY,
+      accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     });
   }
 
   retrieveHomeTimeline() {
-    return this.client.get('statuses/home_timeline', {});
+    return this.client.get('statuses/user_timeline', {screen_name: 'codingstones'})
   }
 }
 
